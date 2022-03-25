51fa6a2f98f33a36b95bbaa216e8357ff39fb1f3
Avinhood <avinashshyam@gmail.com>
2022-03-26 00:04:26 +0530

    Initial commit
    Initial commit

--- AvinNews/app/src/main/java/com/avin/news/ui/BaseActivity.kt
+++ AvinNews/app/src/main/java/com/avin/news/ui/BaseActivity.kt
@@ -2,11 +2,8 @@ package com.avin.news.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -30,33 +27,8 @@ abstract class BaseActivity <T : ViewDataBinding> : AppCompatActivity() {
         val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = DataBindingUtil.inflate(inflater, layoutResourceId, baseBinding.layoutContainer, true)
         baseBinding.ivBack.setOnClickListener({ onBackPressed() })
-        baseBinding.includedErrorLayout.tvErrorRefresh.setOnClickListener({ errorRefreshClicked() })
     }
 
-    protected fun errorRefreshClicked() {}
-
-    fun showToast(msg: String) {
-        if (TextUtils.isEmpty(msg)) {
-            return
-        }
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show()
-    }
-
-    fun showProgress() {
-        baseBinding.progressBar.setVisibility(View.VISIBLE)
-    }
-
-    fun showOverlayProgress() {
-        baseBinding.overlayProgressContainer.setVisibility(View.VISIBLE)
-    }
-
-    fun hideOverlayProgress() {
-        baseBinding.overlayProgressContainer.setVisibility(View.GONE)
-    }
-
-    fun hideProgress() {
-        baseBinding.progressBar.setVisibility(View.GONE)
-    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.getItemId()
@@ -66,43 +38,8 @@ abstract class BaseActivity <T : ViewDataBinding> : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
-    fun setToolbarText(toolbarText: String) {
-        baseBinding.tvToolbarTitle.setText(toolbarText)
-    }
-
-    fun setCustomToolbarText(toolbarText: String, size: Float) {
-        baseBinding.tvToolbarTitle.setText(toolbarText)
-        baseBinding.tvToolbarTitle.setTextSize(size)
-    }
-
-    fun setToolbarVisibility(isVisible: Boolean) {
-        if (!isVisible)
-            baseBinding.toolbar.setVisibility(View.GONE)
-    }
-
     override fun onBackPressed() {
         super.onBackPressed()
-    }
-
-    fun showData() {
-        baseBinding.layoutContainer.setVisibility(View.VISIBLE)
-    }
-
-    fun showError() {
-        baseBinding.layoutError.setVisibility(View.VISIBLE)
-    }
-
-    fun hideError() {
-        baseBinding.layoutError.setVisibility(View.GONE)
-    }
-
-    fun hideBack() {
-        baseBinding.ivBack.setVisibility(View.GONE)
-    }
-
-    fun getData() {
-        baseBinding.layoutContainer.setVisibility(View.VISIBLE)
     }
 
 
