982a974715bd28a53f174a74ddb9a039236a9557
Avinhood <avinashshyam@gmail.com>
2022-03-26 00:14:35 +0530

    Added basic functions

--- AvinNews/app/src/main/java/com/avin/news/ui/MainActivity.kt
+++ AvinNews/app/src/main/java/com/avin/news/ui/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setToolbarText("Top US News")
+        hideBack()
     }
 }
